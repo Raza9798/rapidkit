c287c61f6e61b543539bd349d3346edda5220498
raza9798 <m.raza9798@gmail.com>
2024-06-15 00:55:44 +0530

    commit rapit kit

---
+++ src/Console/Commands/ResourceGeneratorCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Intelrx\Rapidkit\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
+
+class ResourceGeneratorCommand extends Command
+{
+     /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:module {name} {--path=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create new module with controller and model resources. {--path=} is optional.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $name = $this->argument('name');
+        $path = $this->option('path');
+
+        $name = Str::ucfirst($name);
+        if ($path != null) {
+            $this->info("Creating module resources inside: {$path}/{$name}");
+            Artisan::call("make:controller {$path}/{$name}Controller --resource");
+            Artisan::call("make:model {$path}/{$name} -fms");
+            Artisan::call("make:view {$path}/{$name}");
+        }
+        else{
+            $this->info("Creating module resources for {$name}");
+            Artisan::call("make:controller {$name}Controller --resource");
+            Artisan::call("make:model $name -fms");
+            Artisan::call("make:view {$name}");
+
+        }
+        $this->warn('Module command executed successfully!');
+    }
+}
---
+++ src/RapidKitProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Intelrx\Rapidkit;
+
+use Illuminate\Support\ServiceProvider;
+use Intelrx\Rapidkit\Console\Commands\ResourceGeneratorCommand;
+
+class RapidKitProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ResourceGeneratorCommand::class,
+            ]);
+        }
+    }
+}
