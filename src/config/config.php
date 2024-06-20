b9ffc855c72a3fd5a2035d1a93d6c077df71eedf
raza9798 <m.raza9798@gmail.com>
2024-06-20 20:30:08 +0530

    1.8.16

--- src/config/config.php
+++ src/config/config.php
@@ -4,7 +4,7 @@ use Carbon\Carbon;
 
 class Config
 {
-    const VERSION = '1.8.15';
+    const VERSION = '1.8.16';
     const ASSETS_DIR = "/src/assets";
 
     public static function getBuildDir($levels): string
