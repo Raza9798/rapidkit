bc3fb88817c75eb9647bd3e6cd807dbfa982e299
raza9798 <m.raza9798@gmail.com>
2024-07-23 02:48:16 +0530

    version updated

--- src/config/config.php
+++ src/config/config.php
@@ -4,7 +4,7 @@ use Carbon\Carbon;
 
 class Config
 {
-    const VERSION = '1.9.0';
+    const VERSION = '1.9.1';
     const ASSETS_DIR = "/src/assets";
 
     public static function getBuildDir($levels): string
